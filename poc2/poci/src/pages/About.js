30888e02da4a0ae982707700243d350563f4d2a5
SHAUN_PC <SHAUN_PC@SHAUN-PC>
2021-08-05 10:41:30 +0400

    final

--- poc2/poci/src/pages/About.js
+++ poc2/poci/src/pages/About.js
@@ -5,11 +5,14 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
+      As one of the oldest and perhaps most underrated liquors in existence, 
+      gin presents an interesting challenge to first-time drinkers. This botanical booze
+      doesn't often become someone's drink of choice until long after their experimental
+      teen and young adult years. With so few popular college cocktails sweeping campuses 
+      with gin as their base, many people don't learn how to drink gin when they're first 
+      taste testing everything that they can get their hands on. So, if you've been straying 
+      away from trying gin because you're not quite sure where to even start, let this 
+      helpful guide break the junipery ingredient down for you.
       </p>
     </section>
   );
